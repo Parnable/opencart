4f8eaa46d8295c4d8362448ee1e1ecc81716b554
parnable <parnable@gmail.com>
2016-08-09 15:20:48 +0300

    First commit
    Trying to merge my local opencart folder with remote repository

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.test/admin/');
+define('HTTP_CATALOG', 'http://opencart.test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.test/admin/');
+define('HTTPS_CATALOG', 'http://opencart.test/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OpenServer/domains/opencart.test/admin/');
+define('DIR_SYSTEM', 'D:/OpenServer/domains/opencart.test/system/');
+define('DIR_IMAGE', 'D:/OpenServer/domains/opencart.test/image/');
+define('DIR_LANGUAGE', 'D:/OpenServer/domains/opencart.test/admin/language/');
+define('DIR_TEMPLATE', 'D:/OpenServer/domains/opencart.test/admin/view/template/');
+define('DIR_CONFIG', 'D:/OpenServer/domains/opencart.test/system/config/');
+define('DIR_CACHE', 'D:/OpenServer/domains/opencart.test/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OpenServer/domains/opencart.test/system/storage/download/');
+define('DIR_LOGS', 'D:/OpenServer/domains/opencart.test/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OpenServer/domains/opencart.test/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OpenServer/domains/opencart.test/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OpenServer/domains/opencart.test/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
