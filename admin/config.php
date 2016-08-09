fdccb489c4e38bed0b713ffcd00274a0348d0a64
parnable <parnable@gmail.com>
2016-08-09 16:15:54 +0300

    Admin config change

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://opencart.test/admin/');
-define('HTTP_CATALOG', 'http://opencart.test/');
+define('HTTP_SERVER', 'http://opencart/admin/');
+define('HTTP_CATALOG', 'http://opencart/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://opencart.test/admin/');
-define('HTTPS_CATALOG', 'http://opencart.test/');
+define('HTTPS_SERVER', 'http://opencart/admin/');
+define('HTTPS_CATALOG', 'http://opencart/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/OpenServer/domains/opencart.test/admin/');
-define('DIR_SYSTEM', 'D:/OpenServer/domains/opencart.test/system/');
-define('DIR_IMAGE', 'D:/OpenServer/domains/opencart.test/image/');
-define('DIR_LANGUAGE', 'D:/OpenServer/domains/opencart.test/admin/language/');
-define('DIR_TEMPLATE', 'D:/OpenServer/domains/opencart.test/admin/view/template/');
-define('DIR_CONFIG', 'D:/OpenServer/domains/opencart.test/system/config/');
-define('DIR_CACHE', 'D:/OpenServer/domains/opencart.test/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/OpenServer/domains/opencart.test/system/storage/download/');
-define('DIR_LOGS', 'D:/OpenServer/domains/opencart.test/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/OpenServer/domains/opencart.test/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/OpenServer/domains/opencart.test/system/storage/upload/');
-define('DIR_CATALOG', 'D:/OpenServer/domains/opencart.test/catalog/');
+define('DIR_APPLICATION', 'D:/OpenServer/domains/opencart/admin/');
+define('DIR_SYSTEM', 'D:/OpenServer/domains/opencart/system/');
+define('DIR_IMAGE', 'D:/OpenServer/domains/opencart/image/');
+define('DIR_LANGUAGE', 'D:/OpenServer/domains/opencart/admin/language/');
+define('DIR_TEMPLATE', 'D:/OpenServer/domains/opencart/admin/view/template/');
+define('DIR_CONFIG', 'D:/OpenServer/domains/opencart/system/config/');
+define('DIR_CACHE', 'D:/OpenServer/domains/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OpenServer/domains/opencart/system/storage/download/');
+define('DIR_LOGS', 'D:/OpenServer/domains/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OpenServer/domains/opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OpenServer/domains/opencart/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OpenServer/domains/opencart/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
